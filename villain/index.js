3e79a8385bd6885e7a9316034985a162136c6467
jifeng.yao <jifeng.yao@jimubox.com>
2016-01-11 01:43:06 +0800

    update

---
+++ villain/index.js
@@ -0,0 +1,18 @@
+Pace.on("done", function() {
+    var CLS_HIDE = "hide",
+        currentIndex = 0,
+        timer_step1;
+
+    $("#wrap").show();
+
+    var myslider=new iSlider({
+        wrap:'#wrap',
+        item:'.item',
+        onslide:function (index) {}
+    });
+});
+
+$(function() {
+    var myAuto = document.getElementById('myaudio');
+    myAuto.play();
+});
