b7c5326e16e57713df77ec5c98a8a6cb61a92962
jifeng.yao <jifeng.yao@jimubox.com>
2016-12-09 17:12:00 +0800

    f

--- villain/index.js
+++ villain/index.js
@@ -18,11 +18,18 @@ $(function() {
     myAuto.play();
 
     $.ajax({
-        url: "http://api.xiao5market.com/api/jsConfig?url=" + encodeURIComponent(location.href),
+        url: "https://ljspread.hongdianfund.com/wxShare",
         dataType: "jsonp",
+        data: {
+            channel: "muffin",
+            showUrl: encodeURIComponent(location.href)
+        },
         success: function(wxConfig) {
             console.log("success", wxConfig);
-            wx.config(wxConfig);
+            var config = JSON.parse(wxConfig.content);
+            console.log("config : + === ", config);
+            config.debug = true;
+            wx.config(config);
 
             wx.ready(function(){
                 // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
